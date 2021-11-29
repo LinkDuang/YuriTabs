a6e3d081c403223cbf274688b2b2c926ab0d5e15
Yuri <djw7569@gmail.com>
2021-11-29 16:40:55 +0800

    delete not use files

--- script/sheet-url.js
+++
@@ -1,1 +0,0 @@
-const SHEET_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSx54oxuwvA9_ZzgaaTZjwdMVloJBMtH54NdoLARebnUiKYqKS9mbaKHBG4qcF0SzKDRHEGCvgOWeLV/pub?gid=973777172&single=true&output=csv';
