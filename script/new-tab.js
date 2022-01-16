e0d7f000d458412c1485eb2c928bdd8e9e1e8990
Yuri <djw7569@gmail.com>
2022-01-17 00:16:13 +0800

    优化一些加载速度

--- script/new-tab.js
+++ script/new-tab.js
@@ -19,11 +19,8 @@ let rainbowColors = {
   4: 'bg-green-400',
   5: 'bg-cyan-400',
   6: 'bg-sky-400',
-  // ?: 'bg-blue-400',
-  // ?: 'bg-indigo-400',
   7: 'bg-violet-400',
   8: 'bg-purple-400',
-  // ?: 'bg-fuchsia-400',
 }
 
 const genButton = (i, iconStyle, laneIndex) => {
@@ -43,7 +40,7 @@ const genButton = (i, iconStyle, laneIndex) => {
   }
   let dict = {
     icon: `<div class="rounded-full w-5 h-5 mr-2">
-              <img src="${iconUrl}" class="icon-img" />
+              <img src="${iconUrl}" class="icon-img" draggable="false" />
           </div>`,
     gray: `<div class="rounded-full w-5 h-5 mr-2 ${grayColors[laneIndex]}"></div>`,
     rainbow: `<div class="rounded-full w-5 h-5 mr-2 ${rainbowColors[laneIndex]}"></div>`,
@@ -51,7 +48,9 @@ const genButton = (i, iconStyle, laneIndex) => {
 
   //render
   const buttonDom = `
-    <a href=${i.url} class="link-card link-item btn normal-case w-full mb-2 mt-2 btn-outline flex-row flex-nowrap justify-start p-2">
+    <a href=${i.url} 
+      draggable="true" 
+      class="link-card link-item btn normal-case w-full mb-2 mt-2 btn-outline flex-row flex-nowrap justify-start p-2">
       <div class="avatar">
         ${dict[iconStyle]} 
       </div>
@@ -76,17 +75,13 @@ const genLaneWithLaneData = (rawLane, laneIndex) => {
   let iconStyle = localStorage.getItem('iconStyle') // icon | gray | rainbow
 
   let buttonGroup = filted.map((i) => genButton(i, iconStyle, laneIndex))
-  let width = getCardWidth()
-  let margin = getCardSpace()
 
   let laneDom = `
-    <div class="lane ${margin} ${width}">
-      <div class="pl-3 card-title">
-        ${rawLane.title}
-      </div>
-      <div class="">
-        ${buttonGroup.join('')}
-      </div>
+    <div class="pl-3 card-title">
+      ${rawLane.title}
+    </div>
+    <div class="">
+      ${buttonGroup.join('')}
     </div>
   `
   return laneDom
@@ -96,6 +91,9 @@ const __genTabs = () => {
   const container = e('#bookmarks-tree-container')
   container.innerHTML = ''
   let book = chrome.bookmarks
+  let width = getCardWidth()
+  let margin = getCardSpace()
+
   book.getTree((tree) => {
     let page = tree[0].children[0]
     let pool = page.children
@@ -109,17 +107,25 @@ const __genTabs = () => {
         return
       }
       let laneDom = genLaneWithLaneData(lane, laneIndex)
-      container.innerHTML += laneDom
+      // container.innerHTML += laneDom // 更慢一些的方法
+      let div = document.createElement('div')
+      div.className = `lane ${margin} ${width}`
+      div.innerHTML = laneDom
+      container.appendChild(div)
     })
 
     if (other.children.length > 0) {
       let laneDom = genLaneWithLaneData(other, pool.length - 1)
-      container.innerHTML += laneDom
+      // container.innerHTML += laneDom
+      let div = document.createElement('div')
+      div.className = `lane ${margin} ${width}`
+      div.innerHTML = laneDom
+      container.appendChild(div)
     }
   })
 }
 
 window.addEventListener('load', () => {
-  __genTabs()
   e('.square').classList.remove('hidden')
+  __genTabs()
 })
